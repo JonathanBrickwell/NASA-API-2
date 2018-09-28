d7bb37e55efc8109d8b569466897a984850349dc
JonathanBrickwell <jonathanbrickwell@gmail.com>
2018-09-28 22:28:40 +0200

    Kiki : kreiranje tablice, jquery

--- js/asteroids.js
+++ js/asteroids.js
@@ -1,6 +1,3 @@
-
-//var url = "https://api.nasa.gov/planetary/apod?api_key=oAbFeFwglLmUTUFWWYHXm1Xy61sSZmROULa45j72";
-
 var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07&api_key=oAbFeFwglLmUTUFWWYHXm1Xy61sSZmROULa45j72"; 
 
 $(document).ready(function(){
@@ -12,34 +9,16 @@ $(document).ready(function(){
         success: function(result){
 
         var objects = result.near_earth_objects;
-        console.log(objects);
-        
+
         // Prolazak kroz json objekt pomoću petlje.
         // Pristupanje prvog djetetu u objektu pomoću value[1].name.
         $.each(objects, function(index, value){
-          console.log(value[1].name);
+          
+          $("table tbody").append("<tr><td>" + value[1].name + "</td><td>" 
+          + value[1].is_potentially_hazardous_asteroid + "</td><td>"
+          + value[1].estimated_diameter.meters.estimated_diameter_min + " m" + "</td><td>"
+          + value[1].estimated_diameter.meters.estimated_diameter_max + " m" + "</td><td>");
         });
-
-
-        if("copyright" in result) {
-          $("#copyright").text("Image Credits: " + result.copyright);
-        }
-        else {
-          $("#copyright").text("Image Credits: " + "Public Domain");
-        }
-        
-        if(result.media_type == "video") {
-          $("#apod_img_id").css("display", "none"); 
-          $("#apod_vid_id").attr("src", result.url);
-        }
-        else {
-          $("#apod_vid_id").css("display", "none"); 
-          $("#apod_img_id").attr("src", result.url);
-        }
-        $("#reqObject").text(url);
-        $("#returnObject").text(JSON.stringify(result, null, 4));  
-        $("#apod_explaination").text(result.explanation);
-        $('#apod_date').text(result.date);
       }
       });
 
